dfb8b5e3f162692331e19b64399c8fedc40c06c1
alokyadav777 <alokyadav495@gmail.com>
2019-03-24 18:45:35 +0530

    template directory of social media

--- sankalp/production_settings.py
+++ sankalp/production_settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'sankalp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates', 'social_media/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
