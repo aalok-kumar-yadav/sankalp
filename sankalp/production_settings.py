643cd3125afabc8694e9e935146fe54ae09c548a
alokyadav777 <alokyadav495@gmail.com>
2019-03-24 18:43:02 +0530

    media url added in setting

--- sankalp/production_settings.py
+++ sankalp/production_settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
--- sankalp/urls.py
+++ sankalp/urls.py
@@ -31,5 +31,3 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
