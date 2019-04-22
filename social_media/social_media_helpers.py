246bde9cace702e99229b1b98a54d913baee2c6a
alokyadav777 <alokyadav495@gmail.com>
2019-04-22 22:24:59 +0530

    ngo profile error fixed

--- social_media/social_media_helpers.py
+++ social_media/social_media_helpers.py
@@ -15,13 +15,14 @@ from django.db.models import Q
 def get_timeline_context(request, user_id):
     user_name = request.session['username']
     if user_name == user_id:
-        gender_type = "You"
         con_status = "edit"
     else:
-        gender_type = "him" if Contributor.objects.get(user__username=user_id) else "her"
         con_status = "connected"
 
-    session_instance = Contributor.objects.get(user__username=user_id)
+    try:
+        session_instance = Contributor.objects.get(user__username=user_id)
+    except:
+        session_instance = NGO.objects.get(user__username=user_id)
     user_post_list = Post.objects.filter(posted_by__user__username=user_id).order_by('-updated')
     timeline_context = {
         'username': user_name, 'user_info': {'first_name': request.session['first_name'],
@@ -36,17 +37,18 @@ def get_timeline_context(request, user_id):
 def get_timeline_about_context(request, user_id):
     user_name = request.session['username']
     if user_name == user_id:
-        gender_type = "You"
         con_status = "edit"
     else:
-        gender_type = "him" if Contributor.objects.get(user__username=user_id) else "her"
         con_status = "connected"
 
-    contributor_instance = Contributor.objects.get(user__username=user_id)
+    try:
+        contributor_instance = Contributor.objects.get(user__username=user_id)
+    except:
+        contributor_instance = NGO.objects.get(user__username=user_id)
     timeline_context = {
 
         'username': user_name, 'user_info': {'first_name': request.session['first_name'],
-                                           }, 'gender_type': gender_type, 'timeline_section':
+                                           }, 'timeline_section':
             'about', 'user_data': contributor_instance, 'con_status': con_status, 'user_activity': 'None'}
 
     return timeline_context
@@ -78,7 +80,10 @@ def get_connected_people(request, user_id):
         con_status = "connected"
 
     connected_people = Contributor.objects.all()[:8]
-    user_data = Contributor.objects.get(user__username=user_id)
+    try:
+        user_data = Contributor.objects.get(user__username=user_id)
+    except:
+        user_data = NGO.objects.get(user__username=user_id)
     context_data = {'username': request.session['username'],
                     'user_info': {'first_name': request.session['first_name'], 'followers': '204',
                                   'gender_type': "him"}, 'timeline_section': 'connected_people', 'con_status': con_status,
