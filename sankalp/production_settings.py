154361a95d1af0e7fd5091fd8efbf732fbda7005
Aalok Kumar <alokyadav495@gmail.com>
2019-05-14 18:00:32 +0530

    minor changes in production setting

--- sankalp/production_settings.py
+++ sankalp/production_settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ngo_app',
-    'contributor_app'
+    'contributor_app',
+    'social_media'
 ]
 
 MIDDLEWARE = [
