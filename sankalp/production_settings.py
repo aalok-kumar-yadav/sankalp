02a18c8a91cf092fc86fa030bef959e29736868d
Aalok Kumar <alokyadav495@gmail.com>
2019-05-14 18:42:03 +0530

    production humanize

--- sankalp/production_settings.py
+++ sankalp/production_settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ngo_app',
     'contributor_app',
-    'social_media'
+    'social_media',
+    'django.contrib.humanizes'
 ]
 
 MIDDLEWARE = [
